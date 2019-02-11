b10542b9f241b9b3b101e37b624b22380d6367c2
StianIsmar <41960375+StianIsmar@users.noreply.github.com>
2019-02-11 12:21:15 +0100

    Started project

---
+++ closes_value_calc.py
@@ -0,0 +1,16 @@
+# Avoid these frequencies:
+
+freq = {
+		"E" : 82.24,
+		"A" : 110.0,
+		"d" : 146.83,
+		"g" : 195.998
+	}
+
+
+
+def calc_closest_value():
+    print("gdsgdsg")
+
+
+calc_closest_value()
