e545915bc5a4ef57c376753d42f3296427c8345f
StianIsmar <41960375+StianIsmar@users.noreply.github.com>
2019-02-11 13:42:13 +0100

    Algorithm now computes the closest value to each of the four guitar string frequencies

--- closes_value_calc.py
+++ closes_value_calc.py
@@ -7,10 +7,15 @@ freq = {
 		"g" : 195.998
 	}
 
-
-
-def calc_closest_value():
-    print("gdsgdsg")
-
-
-calc_closest_value()
+def calc_closest_value(listOfModes):
+    shortest_distance = [9999,9999,9999,9999]
+    counter = 0
+    for k, v in freq.items():
+        avoid = v
+        for test_mode in listOfModes:
+            if abs(test_mode-v) < shortest_distance[counter]:
+                shortest_distance[counter] = abs(test_mode-v)
+        counter += 1
+    return shortest_distance
+initial_distance = calc_closest_value([81.43,119.821,122.935,136.298,149.285,157.287,173.983,18])
+print(initial_distance)
